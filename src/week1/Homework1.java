3a318ffbcb728b75068250b3b1a20a60cc92c6e4
ilia.greig <ilya.fargo@gmail.com>
2022-07-08 14:30:47 +0500

    Firts task

--- src/week1/Homework1.java
+++ src/week1/Homework1.java
@@ -1,7 +1,22 @@
 package week1;
 
+//С некоторого момента прошло 234 дня. Посчитать:
+//Кол-во полных недель
+//Кол-во месяцев
+
+import java.util.Scanner;
+
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество дней");
+
+        int days = sc.nextInt();
+
+        int fullWeeks = days/7;
+        int fullMonths = days/30;
+
+        System.out.printf("С дня %d прошло %d недель(-и) и %d месяцев", days, fullWeeks, fullMonths);
+
     }
 }
