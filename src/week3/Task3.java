522365a2b48cdd8589bc9e314c7a80f7b157ffc6
ilia.greig <ilya.fargo@gmail.com>
2022-07-11 19:19:56 +0500

    New task

---
+++ src/week3/Task3.java
@@ -0,0 +1,24 @@
+package week3;
+
+
+import java.util.Scanner;
+
+//Дано: найти длину окружности с радиусом 4
+//Формула длины окружности C = 2 * Pi * R
+public class Task3 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double PI = Math.PI;
+
+        System.out.println("Напишите радиус");
+        int Radius = sc.nextInt();
+
+        if (Radius <= 0) {
+            System.out.println("Радиус должен быть натуральным числом! Попробуй еще раз");
+            Radius = sc.nextInt();
+        }
+
+        double C = (2 * PI) * Radius;
+        System.out.printf("Длина окружности с радиусом %d равна %.2f", Radius, C);
+    }
+}
